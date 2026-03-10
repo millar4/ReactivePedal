7b10df38ec96a1aed7ad0c77e009b45ccab05afa
Millar Scandrett <ju22534@bristol.ac.uk>
2026-03-10 04:50:16 +0000

    update

--- ReactivePedal.cpp
+++ ReactivePedal.cpp
@@ -2,9 +2,12 @@
 #include <cmath>
 #include <cstdint>
 #include <cstring>
+#include "daisysp.h"
+#include "arm_math.h"
 
 using namespace daisy;
-
+using namespace std;
+using namespace daisysp;
 
 //create a struct for my results
 struct AudioFeatures
@@ -25,12 +28,16 @@ class FeatureExtractor{
     private:
     static constexpr size_t kMaxFrameSize = 512;
 
+    arm_rfft_fast_instance_f32 fft_;
+    float fftIn_[kMaxFrameSize];
+    float fftOut_[kMaxFrameSize];
     float frame_[kMaxFrameSize];
     float prevMag_[kMaxFrameSize / 2];
     float window_[kMaxFrameSize];
 
     size_t frameSize_ = 0;
     size_t writePos_ = 0;
+    size_t fftSize_ = 0;
     float samplingRate_ = 48000.0f;
 
     void ComputeHannWindow();
@@ -40,12 +47,21 @@ class FeatureExtractor{
 void FeatureExtractor::Init(size_t frameSize, float samplingRate){
     frameSize_ = frameSize;
     samplingRate_ = samplingRate;
+    fftSize_ = frameSize;
     writePos_ = 0;
 
     std::memset(frame_, 0, sizeof(frame_));
     std::memset(prevMag_, 0, sizeof(prevMag_));
+    std::memset(fftIn_, 0, sizeof(fftIn_));
+    std::memset(fftOut_, 0, sizeof(fftOut_));
 
     ComputeHannWindow();
+
+    arm_status result = arm_rfft_fast_init_f32(&fft_, frameSize_);
+        if(result != ARM_MATH_SUCCESS)
+    {
+        printf("ARM FFT FAILED\n");
+    }
 }
 
 bool FeatureExtractor::ProcessSample(float x, AudioFeatures& outFeatures){
@@ -86,6 +102,46 @@ void FeatureExtractor::ComputeFeatures(AudioFeatures& features){
     if(a > maximumAmplitude){
         maximumAmplitude = a;
     }
-    features.peak = maximumAmplitude;
    }
+   features.peak = maximumAmplitude;
+   
+   //Zero crossing rate
+    float runningTotal = 0.0f;
+    for(size_t n = 1; n < frameSize_; n++)
+    {
+        if(frame_[n] * frame_[n-1] < 0)
+            runningTotal += 1.0f;
+    }
+    features.zcr = runningTotal / (frameSize_ - 1);
+
+    //Spectral centroid + flux
+    float runningNum = 0.0f;
+    float runningDen = 0.0f;
+    float spectralTotal = 0.0f;
+    float binWidth = samplingRate_ / frameSize_;
+
+    for(size_t n = 0; n < frameSize_; n++)
+    {
+        fftIn_[n] = frame_[n] * window_[n];
+    }
+
+    arm_rfft_fast_f32(&fft_, fftIn_, fftOut_, 0);
+
+    for(size_t k = 1; k < (fftSize_ / 2); k++)
+    {
+        float real = fftOut_[2 * k];
+        float imaginary = fftOut_[2 * k + 1];
+        float magnitude = sqrtf(real * real + imaginary * imaginary);
+
+        float fk = k * binWidth;
+        runningNum += fk * magnitude;
+        runningDen += magnitude;
+
+        float diff = magnitude - prevMag_[k];
+        spectralTotal += diff * diff;
+        prevMag_[k] = magnitude;
+    }
+
+    features.spectralCentroid = (runningDen > 0.0f) ? (runningNum / runningDen) : 0.0f;
+    features.spectralFlux = spectralTotal;
 }
